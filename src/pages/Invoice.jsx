bc97860a96ec3f5b2eb803fa6d9715d2d5e7273e
Alexis-Torrealba <alexistorrealba1996@gmail.com>
2024-03-12 16:51:05 -0400

    feacture table scroll

--- src/pages/Invoice.jsx
+++ src/pages/Invoice.jsx
@@ -1,122 +1,89 @@
 // Bootstrap
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import Table from 'react-bootstrap/Table'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+import { datas } from '../datebase';
 
 function Invoice() {
 	return (
 		<Container>
-			<h3 style={{ textAlign: 'left' }} className='my-5'>
+			<h3 style={{ textAlign: 'left' }} className="my-5">
 				Invoice
 			</h3>
 			<hr />
 			<Row>
 				{/* Grid 1 */}
-				<div className='w-25'>
+				<div className="w-25">
 					<Col style={{ textAlign: 'left' }}>
-						<Form.Label htmlFor='inputPassword5'>N Factura</Form.Label>
-						<Form.Control type='text' id='invoice' aria-describedby='passwordHelpBlock' />
+						<Form.Control
+							type="text"
+							id="invoice"
+							aria-describedby="passwordHelpBlock"
+							placeholder="Escribe aquí el N° de la factura"
+						/>
 						{/* botones */}
-						<div className='mt-2'>
-							<Button variant='primary'>Consultar</Button>
+						<div className="table-buttons">
+							<Button variant="primary">Consultar</Button>
 						</div>
 					</Col>
 				</div>
 
 				{/* Grid 2 */}
-				<div className='w-75 h-25'>
+				<div className="w-75 h-25">
 					<Col>
-						<Table responsive>
-							<thead>
-								<tr>
-									<th scope='col'>Ref</th>
-									<th scope='col'>Date</th>
-									<th scope='col'>Currency</th>
-									<th scope='col'>TaxIncluded</th>
-									<th scope='col'>NCF</th>
-									<th scope='col'>NCFExpirationDate</th>
-									<th scope='col'>ExchangeRate</th>
-									<th scope='col'>TipoIngreso</th>
-									<th scope='col'>TipoPago</th>
-									<th scope='col'>LinesPerPrintedPage</th>
-									<th scope='col'>SupplierID</th>
-									<th scope='col'>CIF</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<th scope='row'>Mercury</th>
-									<td>4,880</td>
-									<td>0.39</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-								</tr>
-								<tr>
-									<th scope='row'>Mercury</th>
-									<td>4,880</td>
-									<td>0.39</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-								</tr>
-								<tr>
-									<th scope='row'>Mercury</th>
-									<td>4,880</td>
-									<td>0.39</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-								</tr>
-								<tr>
-									<th scope='row'>Mercury</th>
-									<td>4,880</td>
-									<td>0.39</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-									<td>88</td>
-								</tr>
-							</tbody>
-						</Table>
+						<div className="table-container">
+							<Table bordered hover>
+								<thead>
+									<tr className="sticky-top">
+										<th scope="col">Ref</th>
+										<th scope="col">Date</th>
+										<th scope="col">Currency</th>
+										<th scope="col">TaxIncluded</th>
+										<th scope="col">NCF</th>
+										<th scope="col">NCFExpirationDate</th>
+										<th scope="col">ExchangeRate</th>
+										<th scope="col">TipoIngreso</th>
+										<th scope="col">TipoPago</th>
+										<th scope="col">LinesPerPrintedPage</th>
+										<th scope="col">SupplierID</th>
+										<th scope="col">CIF</th>
+									</tr>
+								</thead>
+								<tbody>
+									{datas.map((data, index) => (
+										<tr key={index}>
+											<th scope="row">{data.ref}</th>
+											<td>{data.date}</td>
+											<td>{data.curren}</td>
+											<td>{data.TaxI}</td>
+											<td>{data.ncf}</td>
+											<td>{data.expir}</td>
+											<td>{data.rate}</td>
+											<td>{data.ingres}</td>
+											<td>{data.pay}</td>
+											<td>{data.lines}</td>
+											<td>{data.supplier}</td>
+											<td>{data.cif}</td>
+										</tr>
+									))}
+								</tbody>
+							</Table>
+						</div>
 					</Col>
 				</div>
 			</Row>
 			<hr />
 			<Row>
-				<Col>
-					<Button variant='success'>Generar Facturas</Button>
-					<Button variant='info'>Enviar Facturas</Button>
+				<Col className="table-buttons">
+					<Button variant="success">Generar Facturas</Button>
+					<Button variant="info">Enviar Facturas</Button>
 				</Col>
 			</Row>
 		</Container>
-	)
+	);
 }
 
-export { Invoice }
+export { Invoice };
