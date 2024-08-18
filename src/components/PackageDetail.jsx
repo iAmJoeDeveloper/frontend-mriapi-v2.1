b882bb2ada020c61dfeed81f71b3db17934d62d0
JoeDev <joesuarez.developer@gmail.com>
2024-08-18 17:42:34 -0400

    Check status v0.1

--- src/components/PackageDetail.jsx
+++ src/components/PackageDetail.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { PackageContext } from '../context/PackageContext'
 
@@ -7,12 +7,57 @@ const PackageDetail = () => {
 	const { id } = useParams()
 	const pkg = packages.find((pkg) => pkg._id === id)
 
+	const [intervalId, setIntervalId] = useState(null)
+	const [isRunning, setIsRunning] = useState(false)
+	// const [documentId, setDocumentId] = useState('')
+
+	// Limpia el intervalo cuando el componente se desmonta
+	useEffect(() => {
+		return () => {
+			if (intervalId) {
+				clearInterval(intervalId)
+			}
+		}
+	}, [intervalId])
+
+	const startInterval = () => {
+		// Ejecutar checkDocumentStatus inmediatamente
+		checkDocumentStatus(pkg._id)
+		if (!isRunning) {
+			const id = setInterval(() => {
+				checkDocumentStatus(pkg._id)
+			}, 1 * 60 * 1000) // 5 minutos en milisegundos
+			setIntervalId(id)
+			setIsRunning(true)
+		}
+	}
+
+	const stopInterval = () => {
+		console.log('Interval Stopped')
+		if (isRunning && intervalId) {
+			clearInterval(intervalId)
+			setIntervalId(null)
+			setIsRunning(false)
+		}
+	}
+
 	if (!pkg) {
 		return <div className='font-bold text-2xl'>Package not found</div>
 	}
 
-	const checkStatus = async () => {
-		console.log(pkg)
+	const checkDocumentStatus = async (id) => {
+		// console.log(pkg._id)
+		//'http://localhost:5173//package/checking/:id'
+
+		try {
+			const response = await fetch(`http://localhost:3000/package/checking/${id}`, {
+				'content-type': 'application/json',
+				method: 'GET',
+				//mode: 'no-cors',
+			}).then((response) => console.log(response))
+		} catch (error) {
+			console.error('Error comprobando el estado del documento:', error)
+		}
 	}
 
 	return (
@@ -24,12 +69,21 @@ const PackageDetail = () => {
 				<p className='text-lg font-semibold'>
 					Status: <span className='font-normal'>{pkg.status}</span>
 				</p>
-				<div className='flex flex-col items-center justify-center'>
+				<div className='flex flex-row  space-x-4'>
 					<button
-						onClick={checkStatus}
+						onClick={startInterval}
+						disabled={isRunning}
 						className=' w-3/5 py-2 px-4 text-lg  overflow-hidden  bg-white rounded-lg transition-all duration-400 ease-in-out shadow-md text-black  hover:bg-gray-200'
 					>
 						Check Status
+					</button>
+
+					<button
+						onClick={stopInterval}
+						disabled={!isRunning}
+						className=' w-3/5 py-2 px-4 text-lg  overflow-hidden  bg-white rounded-lg transition-all duration-400 ease-in-out shadow-md text-black  hover:bg-gray-200'
+					>
+						Detener Comprobación
 					</button>
 				</div>
 			</div>
